07000d7a7fb97b91fb5ff7e2889e3f05850051c1
dujuan <dujuan129@gmail.com>
2015-11-11 14:08:16 +0800

    仓库数据库表修改

--- web/db/t_storage.sql
+++ web/db/t_storage.sql
@@ -1,50 +1,51 @@
 /*存放点*/
 DROP TABLE
-IF EXISTS `t_storage_depots`;
+IF EXISTS `t_storage_depot`;
 
-CREATE TABLE `t_storage_depots` (
+CREATE TABLE `t_storage_depot` (
    `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '唯一标示，主键ID',
    `name` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '库存存放点名称',
    `introduction` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '存放点简介',
    `created_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT'创建时间',
    `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT'最后修改时间',
    PRIMARY KEY (`id`)
-) CHARSET=utf8 COMMENT='存放点';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='存放点';
 
 /*库存单据*/
 DROP TABLE
-IF EXISTS `t_storage_document`;
+IF EXISTS `t_storage_report`;
 
-CREATE TABLE `t_storage_documents` (
+CREATE TABLE `t_storage_report` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-	`number` varchar(20) NOT NULL DEFAULT '' COMMENT '编号',
+	`serial_number` varchar(16) NOT NULL DEFAULT '' COMMENT '编号',
 	`comment` varchar(255) NOT NULL DEFAULT '' COMMENT '单据备注',
-	`depots_id` INT(11) NOT NULL DEFAULT '0' COMMENT '存放点ID',
-	`handler` varchar(10) NOT NULL DEFAULT '' COMMENT '经手人',
-	`party_id` INT(11) NOT NULL DEFAULT '0' COMMENT '当事人ID',
+	`depot_id` INT(11) NOT NULL DEFAULT '0' COMMENT '存放点ID',
+	`handler_party_id` varchar(10) NOT NULL DEFAULT '' COMMENT '经手人',
+	`created_party_id` INT(11) NOT NULL DEFAULT '0' COMMENT '当事人ID',
 	`money` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '单据金额',
 	`status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '单据状态：0-未结算、1-已结算',
 	`type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '单据类型：1-入库单、2-出库单、3-盘盈单、4-盘亏单',
 	`created_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最近修改时间',
+  `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最近修改时间',
 	PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='库存单据';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='库存单据';
 
 /*库存单据详细表*/
 DROP TABLE
-IF EXISTS `t_storage_item`;
+IF EXISTS `t_storage_report_item`;
 
-CREATE TABLE `t_storage_item` (
+CREATE TABLE `t_storage_report_item` (
    `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '唯一标示，主键ID',
    `item_id` INT(11) NOT NULL DEFAULT '0' COMMENT '原料ID',
    `quantity` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '入库数量',
    `count` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '计数',
    `price` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '成本价',
-   `document_id` INT(11) NOT NULL DEFAULT '0' COMMENT '单据ID',
+   `comment` varchar(255) NOT NULL DEFAULT '' COMMENT '单据原料详细备注',
+   `report_id` INT(11) NOT NULL DEFAULT '0' COMMENT '单据ID',
    `created_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT'创建时间',
    `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT'最后修改时间',
    PRIMARY KEY (`id`)
-)CHARSET=utf8 COMMENT='库存单据详细';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='库存单据详细';
 
 /*结算表*/
 DROP TABLE
@@ -55,7 +56,7 @@ CREATE TABLE `t_storage_settlement` (
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `last_modified_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='结算表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='结算表';
 
 /*结算详情*/
 DROP TABLE
@@ -80,4 +81,4 @@ CREATE TABLE `t_storage_settlement_item` (
 	`created_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最近修改时间',
 	 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='结算详情';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='结算详情';
