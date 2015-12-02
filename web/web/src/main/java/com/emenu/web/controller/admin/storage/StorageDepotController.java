69693a850f9a5fe3974ce0c7f94409b84631e0bd
dujuan <dujuan129@gmail.com>
2015-12-02 18:29:31 +0800

    注掉了~

--- web/web/src/main/java/com/emenu/web/controller/admin/storage/StorageDepotController.java
+++ web/web/src/main/java/com/emenu/web/controller/admin/storage/StorageDepotController.java
@@ -26,108 +26,108 @@ import java.util.List;
 @RequestMapping(value = URLConstants.ADMIN_STORAGE_DEPOT)
 public class StorageDepotController extends AbstractController{
 
-    /**
-     * 去列表页
-     *
-     * @return
-     */
-    @Module(ModuleEnums.AdminStorageDepotList)
-    @RequestMapping(value = {"","list"},method = RequestMethod.GET )
-    public String toListStorageDepot(){ return "admin/storage/depot/list_home"; }
-
-    /**
-     * ajax获取分页数据
-     *
-     * @param curPage
-     * @param pageSize
-     * @return
-     */
-    @Module(ModuleEnums.AdminStorageDepotList)
-    @RequestMapping(value = "ajax/list/{curPage}",method = RequestMethod.GET)
-    @ResponseBody
-    public JSONObject ajaxListStorageDepot(@PathVariable("curPage")Integer curPage,
-                                           @RequestParam Integer pageSize) {
-        List<StorageDepot> depotList = Collections.<StorageDepot>emptyList();
-        try {
-            depotList = storageDepotService.listByPage(curPage, pageSize);
-        } catch (SSException e) {
-            LogClerk.errLog.error(e);
-            return sendErrMsgAndErrCode(e);
-        }
-
-        JSONArray jsonArray = new JSONArray();
-        for (StorageDepot storageDepot : depotList) {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("id", storageDepot.getId());
-            jsonObject.put("name", storageDepot.getName());
-            jsonObject.put("instruction", storageDepot.getIntroduction());
-
-            jsonArray.add(jsonObject);
-        }
-        int dataCount = 0;
-        try {
-            dataCount = storageDepotService.countAll();
-        } catch (SSException e) {
-            LogClerk.errLog.error(e);
-            sendErrMsgAndErrCode(e);
-        }
-        return sendJsonArray(jsonArray, dataCount);
-    }
-
-    /**
-     * Ajax添加
-     *
-     * @param storageDepot
-     * @return
-     */
-    @Module(ModuleEnums.AdminStorageDepotNew)
-    @RequestMapping(value = "ajax",method = RequestMethod.POST)
-    @ResponseBody
-    public JSONObject ajaxNewStorageDepot(StorageDepot storageDepot) {
-        try {
-            storageDepotService.newStorageDepot(storageDepot);
-            return sendJsonObject(AJAX_SUCCESS_CODE);
-        } catch (SSException e) {
-            LogClerk.errLog.error(e);
-            return sendErrMsgAndErrCode(e);
-        }
-    }
-
-    /**
-     * ajax修改
-     *
-     * @param storageDepot
-     * @return
-     */
-    @Module(ModuleEnums.AdminBasicInfoIndexImgUpdate)
-    @RequestMapping(value= "ajax",method = RequestMethod.PUT)
-    @ResponseBody
-    public JSONObject ajaxUpdateStorageDepot(StorageDepot storageDepot) {
-        try {
-            storageDepotService.updateStorageDepot(storageDepot);
-            return sendJsonObject(AJAX_SUCCESS_CODE);
-        } catch(SSException e) {
-            LogClerk.errLog.error(e);
-            return sendErrMsgAndErrCode(e);
-        }
-    }
-
-    /**
-     * ajax删除
-     *
-     * @param id
-     * @return
-     */
-    @Module(ModuleEnums.AdminStorageDepotDelete)
-    @RequestMapping(value="ajax/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
-    public JSONObject ajaxDelStorageDepot(@PathVariable("id")Integer id) {
-        try {
-            storageDepotService.delById(id);
-            return sendJsonObject(AJAX_SUCCESS_CODE);
-        } catch(SSException e) {
-            LogClerk.errLog.error(e);
-            return sendErrMsgAndErrCode(e);
-        }
-    }
+//    /**
+//     * 去列表页
+//     *
+//     * @return
+//     */
+//    @Module(ModuleEnums.AdminStorageDepotList)
+//    @RequestMapping(value = {"","list"},method = RequestMethod.GET )
+//    public String toListStorageDepot(){ return "admin/storage/depot/list_home"; }
+//
+//    /**
+//     * ajax获取分页数据
+//     *
+//     * @param curPage
+//     * @param pageSize
+//     * @return
+//     */
+//    @Module(ModuleEnums.AdminStorageDepotList)
+//    @RequestMapping(value = "ajax/list/{curPage}",method = RequestMethod.GET)
+//    @ResponseBody
+//    public JSONObject ajaxListStorageDepot(@PathVariable("curPage")Integer curPage,
+//                                           @RequestParam Integer pageSize) {
+//        List<StorageDepot> depotList = Collections.<StorageDepot>emptyList();
+//        try {
+//            depotList = storageDepotService.listByPage(curPage, pageSize);
+//        } catch (SSException e) {
+//            LogClerk.errLog.error(e);
+//            return sendErrMsgAndErrCode(e);
+//        }
+//
+//        JSONArray jsonArray = new JSONArray();
+//        for (StorageDepot storageDepot : depotList) {
+//            JSONObject jsonObject = new JSONObject();
+//            jsonObject.put("id", storageDepot.getId());
+//            jsonObject.put("name", storageDepot.getName());
+//            jsonObject.put("instruction", storageDepot.getIntroduction());
+//
+//            jsonArray.add(jsonObject);
+//        }
+//        int dataCount = 0;
+//        try {
+//            dataCount = storageDepotService.listAll();
+//        } catch (SSException e) {
+//            LogClerk.errLog.error(e);
+//            sendErrMsgAndErrCode(e);
+//        }
+//        return sendJsonArray(jsonArray, dataCount);
+//    }
+//
+//    /**
+//     * Ajax添加
+//     *
+//     * @param storageDepot
+//     * @return
+//     */
+//    @Module(ModuleEnums.AdminStorageDepotNew)
+//    @RequestMapping(value = "ajax",method = RequestMethod.POST)
+//    @ResponseBody
+//    public JSONObject ajaxNewStorageDepot(StorageDepot storageDepot) {
+//        try {
+//            storageDepotService.newStorageDepot(storageDepot);
+//            return sendJsonObject(AJAX_SUCCESS_CODE);
+//        } catch (SSException e) {
+//            LogClerk.errLog.error(e);
+//            return sendErrMsgAndErrCode(e);
+//        }
+//    }
+//
+//    /**
+//     * ajax修改
+//     *
+//     * @param storageDepot
+//     * @return
+//     */
+//    @Module(ModuleEnums.AdminBasicInfoIndexImgUpdate)
+//    @RequestMapping(value= "ajax",method = RequestMethod.PUT)
+//    @ResponseBody
+//    public JSONObject ajaxUpdateStorageDepot(StorageDepot storageDepot) {
+//        try {
+//            storageDepotService.updateStorageDepot(storageDepot);
+//            return sendJsonObject(AJAX_SUCCESS_CODE);
+//        } catch(SSException e) {
+//            LogClerk.errLog.error(e);
+//            return sendErrMsgAndErrCode(e);
+//        }
+//    }
+//
+//    /**
+//     * ajax删除
+//     *
+//     * @param id
+//     * @return
+//     */
+//    @Module(ModuleEnums.AdminStorageDepotDelete)
+//    @RequestMapping(value="ajax/{id}", method = RequestMethod.DELETE)
+//    @ResponseBody
+//    public JSONObject ajaxDelStorageDepot(@PathVariable("id")Integer id) {
+//        try {
+//            storageDepotService.delById(id);
+//            return sendJsonObject(AJAX_SUCCESS_CODE);
+//        } catch(SSException e) {
+//            LogClerk.errLog.error(e);
+//            return sendErrMsgAndErrCode(e);
+//        }
+//    }
 }
