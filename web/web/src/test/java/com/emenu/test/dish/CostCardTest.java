000aec0c6a3d7548966d530517d93d258d77c113
quanyibo <quanyibo@qq.com>
2016-05-17 15:13:42 +0800

    成本卡增删改

--- web/web/src/test/java/com/emenu/test/dish/CostCardTest.java
+++ web/web/src/test/java/com/emenu/test/dish/CostCardTest.java
@@ -2,6 +2,7 @@ package com.emenu.test.dish;
 
 import com.emenu.common.entity.dish.CostCard;
 import com.emenu.mapper.dish.CostCardMapper;
+import com.emenu.service.dish.CostCardService;
 import com.emenu.test.AbstractTestCase;
 import com.pandawork.core.common.exception.SSException;
 import org.junit.Test;
@@ -10,12 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.math.BigDecimal;
 
 /**
- * Created by ALIENWARE on 2016/5/16.
- */
+ * 成本卡测试
+ *
+ * @author: quanyibo
+ * @time: 2016/5/16 9:04
+ **/
 public class CostCardTest extends AbstractTestCase {
 
     @Autowired
     CostCardMapper costCardMapper;
+
+    @Autowired
+    CostCardService costCardService;
 
     @Test
     public void testNewCostCard() throws SSException
@@ -27,13 +34,28 @@ public class CostCardTest extends AbstractTestCase {
         costCard.setAssistCost(new BigDecimal("1.0"));
         costCard.setDeliciousCost(new BigDecimal("1.0"));
         costCard.setStandardCost(new BigDecimal("3.0"));
-        costCardMapper.newCostCard(costCard);
+        costCardService.newCostCard(costCard);
+    }
+
+    @Test
+    public void testUpdateCostCard() throws SSException
+    {
+        CostCard costCard = new CostCard();
+        costCard.setId(1);
+        costCard.setCostCardNumber("4");
+        costCard.setDishId(9);
+        costCard.setMainCost(new BigDecimal("2.0"));
+        costCard.setAssistCost(new BigDecimal("2.0"));
+        costCard.setDeliciousCost(new BigDecimal("2.0"));
+        costCard.setStandardCost(new BigDecimal("6.0"));
+        costCardService.updateCostCard(costCard);
     }
 
     @Test
     public void testDelCostCardById() throws SSException
     {
-        costCardMapper.delCostCardById(2);
+        costCardService.delCostCardById(2);
     }
+
 
 }
