4df57be4888fb868c677db381d636e2442846599
houseyoung <houseyoung@qq.com>
2016-06-20 14:42:50 +0800

    wechat

---
+++ web/service/src/main/java/common/com/emenu/common/enums/wechat/WechatMenuEnums.java
@@ -0,0 +1,10 @@
+package com.emenu.common.enums.wechat;
+
+/**
+ * WechatMenuEnums
+ *
+ * @author: yangch
+ * @time: 2016/6/20 14:42
+ */
+public enum WechatMenuEnums {
+}
---
+++ web/service/src/main/java/service/com/emenu/service/wechat/WechatMessageAdapter.java
@@ -0,0 +1,10 @@
+package com.emenu.service.wechat;
+
+/**
+ * WechatMessageAdapter
+ *
+ * @author: yangch
+ * @time: 2016/6/20 14:42
+ */
+public class WechatMessageAdapter {
+}
