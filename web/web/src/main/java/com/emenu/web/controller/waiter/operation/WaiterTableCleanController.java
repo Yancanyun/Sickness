ee37086551db76d343a83db40b8eb9603a23a85f
houseyoung <houseyoung@qq.com>
2016-06-22 15:43:21 +0800

    服务员清台、换台

--- web/web/src/main/java/com/emenu/web/controller/waiter/operation/WaiterTableCleanController.java
+++ web/web/src/main/java/com/emenu/web/controller/waiter/operation/WaiterTableCleanController.java
@@ -1,12 +1,12 @@
 package com.emenu.web.controller.waiter.operation;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.emenu.common.annotation.IgnoreAuthorization;
+import com.emenu.common.annotation.IgnoreLogin;
 import com.emenu.common.annotation.Module;
-import com.emenu.common.dto.table.AreaDto;
-import com.emenu.common.entity.table.Table;
 import com.emenu.common.enums.other.ModuleEnums;
 import com.emenu.common.enums.table.TableStatusEnums;
+import com.emenu.common.exception.EmenuException;
 import com.emenu.common.utils.URLConstants;
 import com.emenu.web.spring.AbstractAppBarController;
 import com.pandawork.core.common.exception.SSException;
@@ -17,64 +17,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 /**
  * WaiterTableCleanController
  *
  * @author: yangch
  * @time: 2015/12/9 14:53
  */
+@IgnoreLogin
+@IgnoreAuthorization
 @Controller
 @Module(ModuleEnums.WaiterTableClean)
 @RequestMapping(value = URLConstants.WAITER_TABLE_CLEAN_URL)
 public class WaiterTableCleanController extends AbstractAppBarController {
-    /**
-     * Ajax 获取清台首页的数据
-     * @param partyId
-     * @return
-     */
-    @RequestMapping(value = "list", method = RequestMethod.GET)
-    @ResponseBody
-    public JSONObject tableList(@RequestParam("partyId") Integer partyId) {
-        try {
-            //根据PartyId获取餐桌状态为占用已结账的AreaDto
-            List<AreaDto> areaDtoList = waiterTableService.queryAreaDtoByPartyIdAndStatus(partyId, TableStatusEnums.Checkouted.getId());
-
-            JSONArray jsonArray = new JSONArray();
-
-            for (AreaDto areaDto : areaDtoList) {
-                JSONObject jsonObject = new JSONObject();
-
-                jsonObject.put("areaId", areaDto.getArea().getId());
-                jsonObject.put("areaName", areaDto.getArea().getName());
-
-                //获取AreaDto中的Table列表
-                List<Table> tables = areaDto.getTableList();
-
-                JSONArray tableList = new JSONArray();
-
-                int t = 0;
-                for (Table table : tables) {
-                    JSONObject tableJsonObject = new JSONObject();
-                    tableJsonObject.put("tableId", tables.get(t).getId());
-                    tableJsonObject.put("tableName", tables.get(t).getName());
-
-                    tableList.add(tableJsonObject);
-                    t++;
-                }
-
-                jsonObject.put("tableList", tableList);
-                jsonArray.add(jsonObject);
-            }
-
-            return sendJsonArray(jsonArray);
-        } catch (SSException e) {
-            LogClerk.errLog.error(e);
-            return sendErrMsgAndErrCode(e);
-        }
-    }
-
     /**
      * Ajax 执行清台操作
      * @param tableId
@@ -82,10 +36,14 @@ public class WaiterTableCleanController extends AbstractAppBarController {
      */
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject cleanTable(@RequestParam("tableId") Integer tableId) {
+    public JSONObject cleanTable(@RequestParam("partyId") Integer partyId,
+                                 @RequestParam("tableId") Integer tableId) {
         try {
+            // TODO: 根据PartyId记录哪个服务员清的台
+
             tableService.cleanTable(tableId);
-            callCacheService.delTableCallCache(tableId);//清台操作还要把相应的呼叫服务缓存清除
+            callCacheService.delTableCallCache(tableId); // 清台操作还要把相应的呼叫服务缓存清除
+
             return sendJsonObject(AJAX_SUCCESS_CODE);
         } catch (SSException e) {
             LogClerk.errLog.error(e);
--- web/web/src/main/java/com/emenu/web/controller/waiter/table/WaiterTableController.java
+++ web/web/src/main/java/com/emenu/web/controller/waiter/table/WaiterTableController.java
@@ -1,13 +1,11 @@
 package com.emenu.web.controller.waiter.table;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.emenu.common.annotation.IgnoreAuthorization;
 import com.emenu.common.annotation.IgnoreLogin;
 import com.emenu.common.annotation.Module;
 import com.emenu.common.dto.table.AreaDto;
-import com.emenu.common.entity.table.Area;
 import com.emenu.common.entity.table.Table;
 import com.emenu.common.enums.other.ModuleEnums;
 import com.emenu.common.enums.table.TableStatusEnums;
@@ -24,10 +22,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * WaiterTableOpenController
+ * WaiterTableController
  *
  * @author: yangch
  * @time: 2015/12/8 10:01
@@ -55,14 +54,9 @@ public class WaiterTableController extends AbstractAppBarController {
                 throw SSException.get(EmenuException.OperateStatusIsNotLegal);
             }
 
+            // 获取餐台数据
             JSONArray jsonArray = new JSONArray();
-
-            // 根据请求状态不同，返回不同的餐台数据
-            WaiterTableStatusEnums waiterTableStatusEnums = WaiterTableStatusEnums.valueOf(status);
-            switch (waiterTableStatusEnums) {
-                // 开台
-                case OpenTable: jsonArray = openTable(partyId, areaId); break;
-            }
+            jsonArray = getTableList(partyId, areaId, status);
 
             return sendJsonArray(jsonArray);
         } catch (SSException e) {
@@ -72,20 +66,34 @@ public class WaiterTableController extends AbstractAppBarController {
     }
 
     /**
-     * 获取可开台的餐桌的数据
+     * 获取根据状态、PartyId、AreaId获取餐台数据
      * @param partyId
      * @param areaId
      * @return
      */
-    private JSONArray openTable(Integer partyId, Integer areaId) throws SSException {
+    private JSONArray getTableList(Integer partyId, Integer areaId, Integer status) throws SSException {
         JSONArray jsonArray = new JSONArray();
 
         AreaDto areaDto = new AreaDto();
 
         // 若未传来AreaId
         if (Assert.isNull(areaId)) {
-            // 根据PartyId获取餐桌状态为可用的AreaDtoList
-            List<AreaDto> areaDtoList = waiterTableService.queryAreaDtoByPartyIdAndStatus(partyId, TableStatusEnums.Enabled.getId());
+            List<AreaDto> areaDtoList = new ArrayList<AreaDto>();
+            WaiterTableStatusEnums waiterTableStatusEnums = WaiterTableStatusEnums.valueOf(status);
+            switch (waiterTableStatusEnums) {
+                // 开台 根据PartyId获取餐桌状态为可用的AreaDtoList
+                case OpenTable:
+                    areaDtoList = waiterTableService.queryAreaDtoByPartyIdAndStatus(partyId, TableStatusEnums.Enabled.getId());
+                    break;
+                // 清台 根据PartyId获取餐桌状态为占用已结账的AreaDtoList
+                case CleanTable:
+                    areaDtoList = waiterTableService.queryAreaDtoByPartyIdAndStatus(partyId, TableStatusEnums.Checkouted.getId());
+                    break;
+                // 换台 根据PartyId获取餐桌状态为占用未结账的AreaDtoList
+                case ChangeTable:
+                    areaDtoList = waiterTableService.queryAreaDtoByPartyIdAndStatus(partyId, TableStatusEnums.Uncheckouted.getId());
+                    break;
+            }
             // 获取AreaDtoList中的第一个AreaDto
             if (areaDtoList.size() == 0) {
                 return jsonArray;
@@ -95,8 +103,21 @@ public class WaiterTableController extends AbstractAppBarController {
 
         // 若传来AreaId
         else {
-            // 根据PartyId、AreaId获取获取餐桌状态为可用的AreaDto
-            areaDto = waiterTableService.queryAreaDtoByPartyIdAndAreaIdAndStatus(partyId, areaId , TableStatusEnums.Enabled.getId());
+            WaiterTableStatusEnums waiterTableStatusEnums = WaiterTableStatusEnums.valueOf(status);
+            switch (waiterTableStatusEnums) {
+                // 开台 根据PartyId获取餐桌状态为可用的AreaDto
+                case OpenTable:
+                    areaDto = waiterTableService.queryAreaDtoByPartyIdAndAreaIdAndStatus(partyId, areaId, TableStatusEnums.Enabled.getId());
+                    break;
+                // 清台 根据PartyId获取餐桌状态为占用已结账的AreaDto
+                case CleanTable:
+                    areaDto = waiterTableService.queryAreaDtoByPartyIdAndAreaIdAndStatus(partyId, areaId, TableStatusEnums.Checkouted.getId());
+                    break;
+                // 换台 根据PartyId获取餐桌状态为占用未结账的AreaDto
+                case ChangeTable:
+                    areaDto = waiterTableService.queryAreaDtoByPartyIdAndAreaIdAndStatus(partyId, areaId, TableStatusEnums.Uncheckouted.getId());
+                    break;
+            }
         }
 
         //获取AreaDto中的Table列表
@@ -110,6 +131,7 @@ public class WaiterTableController extends AbstractAppBarController {
             jsonObject.put("tableId", table.getId());
             jsonObject.put("tableName", table.getName());
             jsonObject.put("seatNum", table.getSeatNum());
+            jsonObject.put("currentNum", table.getPersonNum());
 
             jsonArray.add(jsonObject);
         }
